f2c9864d39bfeb4ac104608a27bf5900e3b8e38a
Yann <metalyanni@gmail.com>
2025-02-17 23:40:45 -0600

    Agregando manejo de estados en flutter

--- basic_concepts/lib/presentation/screens/counter/counter_screen.dart
+++ basic_concepts/lib/presentation/screens/counter/counter_screen.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -12,15 +19,19 @@ class CounterScreen extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children:[
-            Text('10',style: TextStyle(fontSize: 160 , fontWeight: FontWeight.w100),),
-            Text('Clicks', style: TextStyle(fontSize:25 ))
+          children: [
+            Text(
+              '$clickCounter',
+              style: const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+            ),
+            Text('Clicks', style: TextStyle(fontSize: 25))
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-
+          clickCounter += 1;
+          setState(() {}); //Esto renderiza el widget
         },
         child: Icon(Icons.plus_one),
       ),
